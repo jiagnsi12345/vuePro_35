4514b3366fd908f6cbf6d75346ee761be0854b24
张丝莹 <l>
2019-08-25 16:51:13 +0800

    usercomponet

--- src/view/user/users.vue
+++ src/view/user/users.vue
@@ -1,11 +1,80 @@
 <template>
-    <div class="list">用户列表</div>
+  <div class="list">
+    <!-- 面包屑 -->
+    <el-breadcrumb separator=">">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+      <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 搜索区域 -->
+    <div style="margin-top: 15px; margin-bottom:15px">
+      <el-input placeholder="请输入内容" v-model="userkey" class="input-with-select" style="width:300px">
+        <el-button slot="append" icon="el-icon-search"></el-button>
+      </el-input>
+      <el-button type="success" style="margin-left:15px">添加用户</el-button>
+    </div>
+
+    <!-- 表格展示区域 -->
+    <el-table :data="tableData" style="width: 100% " border>
+      <el-table-column width="50" type="index"></el-table-column>
+      <el-table-column prop="date" label="日期" width="180"></el-table-column>
+      <el-table-column prop="name" label="姓名" width="180"></el-table-column>
+      <el-table-column prop="address" label="地址"></el-table-column>
+      <el-table-column label="状态" width="100">
+        <template>
+          <el-switch v-model="status" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-tooltip class="item" effect="dark" content="编辑" placement="top">
+            <el-button type="primary" icon="el-icon-edit"></el-button>
+          </el-tooltip>
+
+          <el-tooltip class="item" effect="dark" content="分配角色" placement="top">
+            <el-button type="success" icon="el-icon-share"></el-button>
+          </el-tooltip>
+
+          <el-tooltip class="item" effect="dark" content="删除" placement="top">
+            <el-button type="warning" icon="el-icon-delete"></el-button>
+          </el-tooltip>
+        </template>
+      </el-table-column>
+    </el-table>
+    <!-- 分页区域 -->
+  </div>
 </template>
 <script>
 export default {
-
+  data () {
+    return {
+      userkey: '',
+      status: true,
+      tableData: [
+        {
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },
+        {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄'
+        },
+        {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        },
+        {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }
+      ]
+    }
+  }
 }
 </script>
 <style scoped>
-
 </style>
